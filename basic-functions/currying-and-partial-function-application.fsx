d32ede75f9b4fa7e2a578b16992055005f0262ea
demirtas-dev <62238518+demirtas-dev@users.noreply.github.com>
2020-05-23 09:37:42 +0100

    Currying and partial function application.

---
+++ basic-functions/currying-and-partial-function-application.fsx
@@ -0,0 +1,9 @@
+﻿let curriedPrint header value = printfn "%s %d" header value
+let tupledPrint (header, value) = printfn "%s %d" header value
+
+curriedPrint "The answer is" 42
+
+tupledPrint ("The answer is", 42)
+
+let curriedStepOne = curriedPrint "The answer is"
+let curriedStepTwo = curriedStepOne 42
