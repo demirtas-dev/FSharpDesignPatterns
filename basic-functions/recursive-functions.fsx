acb4333bdf5c41ea8d542b936e7330e49c3d65aa
demirtas-dev <62238518+demirtas-dev@users.noreply.github.com>
2020-05-23 16:41:54 +0100

    Recursive functions and operators as functions

---
+++ basic-functions/recursive-functions.fsx
@@ -0,0 +1,23 @@
+﻿// compare the behavior of:
+let cutter1 s =
+    let cut s = printfn "imitator cut: %s" s
+    let cut (s : string) =
+        if s.Length > 0 then
+            printfn "real cut: %s" s
+            cut s.[1..]
+        else
+            printfn "finished cutting"
+    cut s
+// with:
+let cutter2 s =
+    let cut s = printfn "imitator cut: %s" s
+    let rec cut (s : string) =
+        if s.Length > 0 then
+            printfn "real cut: %s" s
+            cut s.[1..]
+        else
+            printfn "finished cutting"
+    cut s
+// all that changes is the `rec` keyword
+cutter1 "Noluyor?"
+cutter2 "Noluyor?"
