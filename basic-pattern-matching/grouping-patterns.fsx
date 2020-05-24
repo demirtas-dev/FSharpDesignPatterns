67f5ce9f7f18af8169a8ae7194f65c334c6fedff
demirtas-dev <62238518+demirtas-dev@users.noreply.github.com>
2020-05-24 01:44:45 +0100

    Grouping patterns.

---
+++ basic-pattern-matching/grouping-patterns.fsx
@@ -0,0 +1,12 @@
+﻿open System
+
+let validate keyA keyB =
+    match (keyA,keyB) with
+    | ("","") -> "Both keys are empty."
+    | (x,y) & (("",_) | (_,"")) -> sprintf "One key is empty. keyA: %s. keyB: %s" keyA keyB
+    | _ & (x,y) -> sprintf "Both keys are NOT empty. keyA: %s. keyB: %s." keyA keyB
+    
+validate "" ""
+validate "" "y"
+validate "x" ""
+validate "x" "y"
