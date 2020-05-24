12c219b8a9a3cc64eadddce9317673f47dfa5f18
demirtas-dev <62238518+demirtas-dev@users.noreply.github.com>
2020-05-24 01:19:04 +0100

    Matching literals.

---
+++ basic-pattern-matching/matching-literals.fsx
@@ -0,0 +1,21 @@
+﻿[<Literal>]
+let THREE = 3
+
+let transformA v =
+    match v with
+    | 1 -> "1"
+    | 2 -> "2"
+    | THREE -> "3"
+    
+transformA (1 + 2)
+
+type Multiples =
+    | Zero = 0
+    | Five = 5
+
+let transformB ``compare me`` =
+    match ``compare me`` with
+    | Multiples.Zero -> "0"
+    | Multiples.Five -> "5"
+
+transformB Multiples.Five
