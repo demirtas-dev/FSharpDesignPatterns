a7a6a77c754e8987229e491df61945f2b073c09e
demirtas-dev <62238518+demirtas-dev@users.noreply.github.com>
2020-05-24 08:23:39 +0100

    Sequence functions: aggregates and generators.

---
+++ algebraic-data-types/sum-algebraic-data-types.fsx
@@ -0,0 +1,40 @@
+﻿module DiscriminatedUnions =
+    [<AutoOpen>]
+    module Composition =
+        type ChargeAttempt =
+            | Original
+            | Retry of int
+            
+        let cco = Original
+        let ccr = Retry 4
+        
+        type Brightness = Brightness of int
+        type Voltage = Voltage of int
+        type Bulb = { brightness : Brightness ; voltage : Voltage }
+        
+        let myBulb = { brightness = Brightness 110 ; voltage = Voltage 2500 }
+        
+    [<AutoOpen>]
+    module EqualityAndComparison =
+        let lamp1br = Brightness 2500
+        lamp1br = Brightness 2500
+        lamp1br < Brightness 2100
+        
+    [<AutoOpen>]
+    module Decomposition =
+        match myBulb.brightness with
+        | Brightness v -> v
+    
+    [<AutoOpen>]
+    module Augmentation =
+        type PaymentInstrumentDiscount =
+            | CreditCard of decimal
+            | DebitCard of decimal
+            | ACH of decimal
+            
+            member this.ApplyDiscount payment =
+                match this with
+                | CreditCard discount -> payment - discount
+                | DebitCard discount -> payment - discount
+                | ACH discount -> payment - discount
+        
---
+++ sequences/sequence-functions.fsx
@@ -0,0 +1,40 @@
+﻿module Aggregates =
+    //average : seq<^T> -> ^T (requires member (+) and member DivideByInt and member get_Zero)
+    seq { 1. .. 4. } |> Seq.average
+    //averageBy : ('T -> ^U) -> seq<'T> -> ^U (requires ^U with static member (+) and ^U with static member DivideByInt and ^U with static member Zero)
+    seq { "1" ; "2" ; "3" } |> Seq.averageBy (double >> (+) 50.)
+    //fold : ('State -> 'T -> 'State) -> 'State -> seq<'T> -> 'State
+    seq { "X" ; "y" ; "z" ; "z" ; "y" } |> Seq.fold (fun a x -> a + (x |> function "z" -> 1 | _ -> 0)) 0
+    //length : seq<'T> -> int
+    seq { 1 .. 12 } |> Seq.length
+    //sum : seq<^T> -> ^T (requires member (+) and member get_Zero)
+    seq { 1 .. 4 } |> Seq.sum
+    //sumBy : ('T -> ^U) -> seq<'T> -> ^U (requires ^U with static member (+) and 'U with static member Zero)
+    seq { "1" ; "2" ; "3" ; "4" } |> Seq.sumBy (((+) "10") >> double)
+    //max : seq<'T> -> 'T (requires comparison)
+    seq { 11 ; 20 ; 3 } |> Seq.max
+    //maxBy : ('T -> 'U) -> Seq<'T> -> 'U (requires comparison)
+    seq { "11" ; "20" ; "3" } |> Seq.maxBy int
+    //min : seq<'T> -> 'T (requires comparison)
+    seq { "11" ; "20" ; "3" } |> Seq.min
+    //minBy : ('T -> 'U) -> Seq<'T> -> 'U (requires comparison)
+    seq { "11" ; "20" ; "3" } |> Seq.minBy float
+    //isEmpty : seq<'T> -> bool
+    Seq.isEmpty Seq.empty
+    //reduce : ('T -> 'T -> 'T) -> seq<'T> -> 'T
+    seq { "X" ; "y" ; "z" ; "z" ; "y" } |> Seq.reduce (fun a x -> a + x)
+    //exactlyOne: seq<'T> -> 'T
+    Seq.exactlyOne [ "42" ]
+
+module Generators =
+    //empty : seq<'T>
+    Seq.empty
+    //init : int -> (int -> 'T) -> seq<'T>
+    Seq.init 5 ((*) 10)
+    //initInfinite : (int -> 'T) -> seq<'T>
+    Seq.initInfinite ((*) 100 >> string)
+    //singleton : 'T -> seq<'T>
+    seq { '*' ; '.' } |> Seq.singleton 
+    //unfold : ('State -> 'T * 'State option) -> 'State -> seq<'T>
+    Seq.unfold (fun current -> Some(fst current + snd current, (snd current, fst current + snd current))) (1 , 1)
+    
