aa71fa396e650295be42f4b6cd3217695cba4cd9
demirtas-dev <62238518+demirtas-dev@users.noreply.github.com>
2020-05-24 01:51:21 +0100

    Alternative syntax for anonymous function that performs matching.

---
+++ basic-pattern-matching/anonymous-matching.fsx
@@ -0,0 +1,9 @@
+﻿open System
+
+let validate keyA keyB = (keyA, keyB) |> function
+    | ("","") -> "Both keys are empty."
+    | (x,y) & (("",_) | (_,"")) -> sprintf "One key is empty. keyA: %s. keyB: %s" keyA keyB
+    | (x,y) when x = y -> sprintf "Both keys are equal and NOT empty: %s" x
+    | _ & (x,y) -> sprintf "Both keys are NOT empty. keyA: %s. keyB: %s." keyA keyB
+
+validate "sa" "sa"
