ae7070f166eff711dccafafdef617ac2ff5a2100
demirtas-dev <62238518+demirtas-dev@users.noreply.github.com>
2020-05-24 01:49:13 +0100

    `When` guard.

---
+++ basic-pattern-matching/guards.fsx
@@ -0,0 +1,10 @@
+﻿open System
+
+let validate keyA keyB =
+    match (keyA,keyB) with
+    | ("","") -> "Both keys are empty."
+    | (x,y) & (("",_) | (_,"")) -> sprintf "One key is empty. keyA: %s. keyB: %s" keyA keyB
+    | (x,y) when x = y -> sprintf "Both keys are equal and NOT empty: %s" x
+    | _ & (x,y) -> sprintf "Both keys are NOT empty. keyA: %s. keyB: %s." keyA keyB
+
+validate "sa" "sa"    
