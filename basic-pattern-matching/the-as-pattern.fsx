e2c29c396fb776364640d0c0554f8f45d0a1f895
demirtas-dev <62238518+demirtas-dev@users.noreply.github.com>
2020-05-24 01:36:58 +0100

    The "as" pattern.

---
+++ basic-pattern-matching/the-as-pattern.fsx
@@ -0,0 +1,8 @@
+﻿let verifyGuid (g : string) =
+    match System.Guid.TryParse g with
+    | (true, _ as r) -> sprintf "%s is a genuine GUID: %A" g (snd r)
+    | (_, _ as r) -> sprintf "%s is a garbage GUID, defaults to %A" g (snd r)
+    
+verifyGuid (System.Guid.NewGuid().ToString())
+verifyGuid "SADFweasdfgaklfsdj-fasdFASDFAS-sfafsad"
+
